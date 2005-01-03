1deaf6b0b8fae28f75a3bc22e7ca2a5c75034083
Paul Eggert <eggert@cs.ucla.edu>
2005-01-03 07:40:10 +0000

    Update copyright date to 2005.

--- tests/mktests.sh
+++ tests/mktests.sh
@@ -229,7 +229,7 @@ do
     ## Do not edit by hand.  ##
     ## --------------------- ##
 
-    # Copyright (C) 2000, 2001, 2003, 2004 Free Software Foundation, Inc.
+    # Copyright (C) 2000, 2001, 2003, 2004, 2005 Free Software Foundation, Inc.
 
     AT_BANNER([Testing autoconf/$base macros.])
 
