681d17dce2aa6c1258318b0400b2ab22aadffe84
Paul Eggert <eggert@cs.ucla.edu>
2005-05-14 07:00:38 +0000

    Update FSF postal mail address.

--- lib/emacs/autoconf-mode.el
+++ lib/emacs/autoconf-mode.el
@@ -20,8 +20,8 @@
 ;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-;; 02111-1307, USA.
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
 
 ;; A major mode for editing autoconf input (like configure.in).
 ;; Derived from m4-mode.el by Andrew Csillag (drew@staff.prodigy.com)
--- tests/mktests.sh
+++ tests/mktests.sh
@@ -17,8 +17,8 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-# 02111-1307, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
 # If we fail, clean up, but touch the output files.  We probably failed
 # because we used some non portable tool, but we just don't care: this
