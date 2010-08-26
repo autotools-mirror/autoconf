autotools-mirror/autoconf

commit e0ac12089ea4c934029baf77741e659f0bebd653

m4sh: fix some namespace safety issues
* lib/m4sugar/m4sh.m4 (_AS_SHELL_SANITIZE): Avoid problems if
as_myself is inherited from environment.
(AS_TMPDIR): Be namespace clean.

Signed-off-by: Eric Blake <eblake@redhat.com>

lib/m4sugar/m4sh.m4
@@ -425,6 +425,7 @@ _AS_PATH_SEPARATOR_PREPARE
 IFS=" ""	$as_nl"
 
 # Find who we are.  Look in the path if we contain no directory separator.
+as_myself=
 case $[0] in @%:@((
   *[[\\/]]* ) as_myself=$[0] ;;
   *) _AS_PATH_WALK([],
@@ -1618,12 +1619,13 @@ m4_define([AS_TMPDIR],
 [# Create a (secure) tmp directory for tmp files.
 m4_if([$2], [], [: ${TMPDIR=/tmp}])
 {
-  tmp=`(umask 077 && mktemp -d "m4_default([$2], [$TMPDIR])/$1XXXXXX") 2>/dev/null` &&
-  test -n "$tmp" && test -d "$tmp"
+  as_tmp=`(umask 077 && mktemp -d "m4_default([$2],
+    [$TMPDIR])/$1XXXXXX") 2>/dev/null` &&
+  test -d "$as_tmp"
 }  ||
 {
-  tmp=m4_default([$2], [$TMPDIR])/$1$$-$RANDOM
-  (umask 077 && mkdir "$tmp")
+  as_tmp=m4_default([$2], [$TMPDIR])/$1$$-$RANDOM
+  (umask 077 && mkdir "$as_tmp")
 } || AS_ERROR([cannot create a temporary directory in m4_default([$2],
 	      [$TMPDIR])])])# AS_TMPDIR
 

commit 4b6f3d2ce1096f377d290136b6c84bcf60e786ec

m4sh: revert regression in AS_TMPDIR
Regression introduced in e0ac12089ea4c934029baf77741e659f0bebd653.

* lib/m4sugar/m4sh.m4 (AS_TMPDIR): The previous patch trying to
rename $tmp to $as_tmp was wrong; config.status relies on it.

Signed-off-by: Eric Blake <eblake@redhat.com>

lib/m4sugar/m4sh.m4
@@ -1615,17 +1615,20 @@ m4_define([_AS_LITERAL_HEREDOC_IF_NO], [$2])
 # -------------------------------------------------
 # Create as safely as possible a temporary directory in DIRECTORY
 # which name is inspired by PREFIX (should be 2-4 chars max).
+#
+# Even though $tmp does not fit our normal naming scheme of $as_*,
+# it is a documented part of the public API and must not be changed.
 m4_define([AS_TMPDIR],
 [# Create a (secure) tmp directory for tmp files.
 m4_if([$2], [], [: "${TMPDIR=/tmp}"])
 {
-  as_tmp=`(umask 077 && mktemp -d "m4_default([$2],
+  tmp=`(umask 077 && mktemp -d "m4_default([$2],
     [$TMPDIR])/$1XXXXXX") 2>/dev/null` &&
-  test -d "$as_tmp"
+  test -d "$tmp"
 }  ||
 {
-  as_tmp=m4_default([$2], [$TMPDIR])/$1$$-$RANDOM
-  (umask 077 && mkdir "$as_tmp")
+  tmp=m4_default([$2], [$TMPDIR])/$1$$-$RANDOM
+  (umask 077 && mkdir "$tmp")
 } || AS_ERROR([cannot create a temporary directory in m4_default([$2],
 	      [$TMPDIR])])])# AS_TMPDIR
 